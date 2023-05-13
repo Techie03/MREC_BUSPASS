1792943ea3e25f879ac86bfe6bb97622bcb7a72b
MAREDDY NISHITH CHOWDARY <96654142+Techie03@users.noreply.github.com>
2023-05-13 19:47:56 +0530

    Add files via upload

---
+++ buspassdb.sql
@@ -0,0 +1,218 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 13, 2023 at 07:41 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `buspassdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbladmin`
+--
+
+CREATE TABLE `tbladmin` (
+  `ID` int(10) NOT NULL,
+  `AdminName` varchar(120) DEFAULT NULL,
+  `UserName` varchar(120) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Email` varchar(120) DEFAULT NULL,
+  `Password` varchar(200) DEFAULT NULL,
+  `AdminRegdate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbladmin`
+--
+
+INSERT INTO `tbladmin` (`ID`, `AdminName`, `UserName`, `MobileNumber`, `Email`, `Password`, `AdminRegdate`) VALUES
+(1, 'MREC TRANSPORT ', 'admin', 9393007192, 'transport@mrec.ac.in', 'f925916e2754e5e03f75dd58a5733251', '2022-10-26 18:30:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcategory`
+--
+
+CREATE TABLE `tblcategory` (
+  `ID` int(10) NOT NULL,
+  `CategoryName` varchar(200) DEFAULT NULL,
+  `CreationDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tblcategory`
+--
+
+INSERT INTO `tblcategory` (`ID`, `CategoryName`, `CreationDate`) VALUES
+(8, '2032', '2021-07-04 14:27:53'),
+(9, '2129', '2021-07-04 14:28:32'),
+(10, '6152', '2021-07-04 14:28:47'),
+(11, '3199', '2021-07-04 14:29:01');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcontact`
+--
+
+CREATE TABLE `tblcontact` (
+  `ID` int(10) NOT NULL,
+  `Name` varchar(200) DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `Message` mediumtext DEFAULT NULL,
+  `EnquiryDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `IsRead` int(5) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tblcontact`
+--
+
+INSERT INTO `tblcontact` (`ID`, `Name`, `Email`, `Message`, `EnquiryDate`, `IsRead`) VALUES
+(3, 'nishith', 'nishith374@gmail.com', 'hii', '2022-10-29 05:10:14', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblpage`
+--
+
+CREATE TABLE `tblpage` (
+  `ID` int(10) NOT NULL,
+  `PageType` varchar(200) DEFAULT NULL,
+  `PageTitle` varchar(200) DEFAULT NULL,
+  `PageDescription` mediumtext DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `UpdationDate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tblpage`
+--
+
+INSERT INTO `tblpage` (`ID`, `PageType`, `PageTitle`, `PageDescription`, `Email`, `MobileNumber`, `UpdationDate`) VALUES
+(1, 'aboutus', 'About us', 'College Buses ply exclusively for students connecting main locations in the city in addition to availability of RTC buses.\r\nIt was launched in August 2002. Within a short span of time it has acquired a good fleet of buses. On time and service are the core value differentiators, thereby making it an unmatched travelling experience.\r\n\r\nTransport Supervioser\r\nMr Sardar Bhupender Singh\r\nTel: 7989620416\r\n\r\nTransport Manager\r\nMr. S Prasad Garu\r\nTel: 9393007192\r\n\r\n', 'Email: transport@mrec.ac.in', 9393007192, '2022-10-26 18:30:00'),
+(2, 'contactus', 'Contact Us', 'Maisammaguda, Dhulapally, Kompally, Medchal India, Hyderabad, Telangana 500055', 'transport@mrec.ac.in', 7989620416, '2022-10-26 18:30:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblpass`
+--
+
+CREATE TABLE `tblpass` (
+  `ID` int(10) NOT NULL,
+  `PassNumber` varchar(200) DEFAULT NULL,
+  `FullName` varchar(200) DEFAULT NULL,
+  `ProfileImage` varchar(200) DEFAULT NULL,
+  `ContactNumber` bigint(10) DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `IdentityType` varchar(200) DEFAULT NULL,
+  `IdentityCardno` varchar(200) DEFAULT NULL,
+  `Category` varchar(100) DEFAULT NULL,
+  `Source` varchar(200) DEFAULT NULL,
+  `Destination` varchar(200) DEFAULT NULL,
+  `FromDate` varchar(200) DEFAULT NULL,
+  `ToDate` varchar(200) DEFAULT NULL,
+  `Cost` decimal(10,0) DEFAULT NULL,
+  `PasscreationDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tblpass`
+--
+
+INSERT INTO `tblpass` (`ID`, `PassNumber`, `FullName`, `ProfileImage`, `ContactNumber`, `Email`, `IdentityType`, `IdentityCardno`, `Category`, `Source`, `Destination`, `FromDate`, `ToDate`, `Cost`, `PasscreationDate`) VALUES
+(7, '278089945', 'MAREDDY NISHITH CHOWDARY', '3896fe87c13710411786a4221ca8c8c81666855452.jpg', 9866130604, 'nishith374@gmail.com', 'Student Card', '20j41a6927', '2032', 'BHEL', 'MREC', '2022-10-27', '2023-10-26', '35000', '2022-10-27 07:24:12'),
+(8, '153695462', 'sai majeti', '3896fe87c13710411786a4221ca8c8c81667021549.jpg', 6300836685, 'majetisai@gmail.com', 'Student Card', '20J41A6738', '2032', 'ALWAL', 'MREC', '2022-10-29', '2023-10-28', '35000', '2022-10-29 05:32:29');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblcategory`
+--
+ALTER TABLE `tblcategory`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblcontact`
+--
+ALTER TABLE `tblcontact`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblpage`
+--
+ALTER TABLE `tblpage`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblpass`
+--
+ALTER TABLE `tblpass`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tblcategory`
+--
+ALTER TABLE `tblcategory`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `tblcontact`
+--
+ALTER TABLE `tblcontact`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tblpage`
+--
+ALTER TABLE `tblpage`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tblpass`
+--
+ALTER TABLE `tblpass`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
