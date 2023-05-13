d8a94d6de476dd657ea59c185bc0bacb6d7a9d96
MAREDDY NISHITH CHOWDARY <96654142+Techie03@users.noreply.github.com>
2023-05-13 19:44:34 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+
+	<div class="copy-right"> 
+		<div class="container">
+			<p>© Malla Reddy Engineering College    ||  All Rights Reserved</p>
+		</div> 
+	</div> 
+	<!-- //footer -->   
+	
---
+++ includes/header.php
@@ -0,0 +1,63 @@
+<div class="top-nav w3-agiletop">
+                <div class="container">
+                    <div class="navbar-header w3llogo">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>  
+                        <h1><a href="index.php">MREC BUS PASS</a></h1> 
+                    </div>
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <div class="w3menu navbar-right">
+                            <ul class="nav navbar">
+                                <li><a href="index.php" class="active">Home</a></li>
+                                <li><a href="about.php">About</a></li> 
+                                <li><a href="contact.php">Contact</a></li>
+                                <li><a href="download-pass.php">View Pass</a></li>
+                                <li><a href="admin/index.php">Admin</a></li>
+                            </ul>
+                        </div> 
+                        <div class="clearfix"> </div>  
+                    </div>
+                </div>  
+            </div>
+
+<style>
+.scroll-left {
+ height: 30px;	
+ overflow: hidden;
+ position: relative;
+ background:yellow;
+ color: orange;
+ border: 1px solid orange;
+}
+.scroll-left p {
+ position: absolute;
+ width: 100%;
+ height: 100%;
+ margin: 0;
+ line-height: 30px;
+ text-align: center;
+ /* Starting position */
+ transform:translateX(100%);
+ /* Apply animation to this element */
+ animation: scroll-left 15s linear infinite;
+}
+/* Move it (define the animation) */
+@keyframes scroll-left {
+ 0%   {
+ transform: translateX(100%); 		
+ }
+ 100% {
+ transform: translateX(-100%); 
+ }
+}
+</style>
+
+<div class="scroll-left">
+<p>**Welcome To MREC Bus Pass Website****Welcome To MREC Bus Pass Website****Welcome To MREC Bus Pass Website****Welcome To MREC Bus Pass Website****Welcome To MREC Bus Pass Website****Welcome To MREC Bus Pass Website****Welcome To MREC Bus Pass Website****Welcome To MREC Bus Pass Website****Welcome To MREC Bus Pass Website****Welcome To MREC Bus Pass Website****Welcome To MREC Bus Pass Website****Welcome To MREC Bus Pass Website**</p>
+</div>
